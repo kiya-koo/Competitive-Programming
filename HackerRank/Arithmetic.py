bd4e7b01ff4df62ce9ec6d505efcacbf59e0c0ff
kiya <92818425+kiya-koo@users.noreply.github.com>
2022-10-05 00:12:55 +0300

    Create Arithmetic.py

---
+++ HackerRank/Arithmetic.py
@@ -0,0 +1,10 @@
+import math
+a = int(input())
+b = int(input())
+if a >= 1 and a <= pow(10,10) and b >= 1 and b <= pow(10,10):
+    add = a + b
+    sub = a - b
+    mul = a * b
+    print(add)
+    print(sub)
+    print(mul)
